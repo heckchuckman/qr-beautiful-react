a17d538d495b575b678a3401eff0c99769702fb1
heckchuckman <chuck.heckman@gmail.com>
2023-06-07 16:56:45 -0400

    updated packages and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        libraryTarget: 'umd',
-        library: 'react-qrbtf',
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-    ],
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                test: /\.js$/,
-            }),
-        ]
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'awesome-typescript-loader',
-            exclude: /node_modules/,
-        }]
-    },
-    externals: {
-        react: 'react',
-        'react-dom': 'react-dom'
-    }
+	entry: "./src/index.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index.js",
+		libraryTarget: "umd",
+		library: "react-qrbtf",
+		umdNamedDefine: true,
+		clean: true,
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+	},
+	// plugins: [new CleanWebpackPlugin()],
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				test: /\.js$/,
+			}),
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	externals: {
+		react: "react",
+		"react-dom": "react-dom",
+	},
 }
