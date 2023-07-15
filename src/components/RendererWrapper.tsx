1fcfba7e5cbbe371af1caaeceec7736cc0ee4afa
heckchuckman <chuck.heckman@gmail.com>
2023-07-15 15:43:36 -0400

    small change to default URL

--- src/components/RendererWrapper.tsx
+++ src/components/RendererWrapper.tsx
@@ -1,81 +1,141 @@
-import React from 'react';
-import { encodeData } from "../utils/qrcodeHandler";
-import QRCode from "../utils/qrcode";
-import reactCSS from "reactcss";
-import merge from "lodash/merge";
+import React from "react"
+import { encodeData } from "../utils/qrcodeHandler"
+import QRCode from "../utils/qrcode"
+import reactCSS from "reactcss"
+import merge from "lodash/merge"
 
 export interface RendererProps {
-    qrcode?: QRCode,
-    className?: string,
-    value?: string,
-    level?: string,
-    styles?: any,
-    title?: string,
-    titleSize?: number,
-    titleColor?: string,
-    titleAlign?: string,
-    icon?: string,
-    iconScale?: number,
-};
-
-export type SFC<P = {}> = StyledFunctionComponent<P>;
-
-export interface StyledFunctionComponent<P = {}> extends React.FunctionComponent<P> {
-    defaultCSS?: any
-};
-
-export const RendererWrapper = <T extends RendererProps>(renderer: SFC<T>) => {
-    const Renderer: SFC<T> = (props: T) => {
-        let newProps: T = Object.assign({}, props);
-
-        newProps.value = newProps.value || "https://qrbtf.com";
-        newProps.level = newProps.title || newProps.icon ? "H" : newProps.level || 'M'
-        newProps.qrcode = newProps.qrcode || encodeData({ text: newProps.value, correctLevel: newProps.level, typeNumber: -1 });
-        newProps.styles = reactCSS(merge({
-            'default': renderer.defaultCSS
-        }, {
-            'default': props.styles
-        }));
-
-        return (
-            React.createElement(renderer, newProps)
-        );
-    }
-    return Renderer;
+	qrcode?: QRCode
+	className?: string
+	value?: string
+	level?: string
+	styles?: any
+	title?: string
+	titleSize?: number
+	titleColor?: string
+	titleAlign?: string
+	icon?: string
+	iconScale?: number
 }
 
-export function drawIcon({ qrcode, title, titleSize, titleColor, titleAlign, icon, iconScale = .33, styles }: RendererProps) {
-    if (!qrcode) return []
-    if (!title && !icon) return null;
+export type SFC<P = {}> = StyledFunctionComponent<P>
 
-    const nCount = qrcode.getModuleCount();
-    const { fontSize, color, verticalAlign, ...titleStyle } = styles.title || {};
-    const titleVerticalAlign = titleAlign || verticalAlign || (icon ? "bottom" : "middle");
+export interface StyledFunctionComponent<P = {}>
+	extends React.FunctionComponent<P> {
+	defaultCSS?: any
+}
 
-    iconScale = iconScale > .33 ? .33 : iconScale;
-    const iconSize = Number(nCount * iconScale);
-    const iconXY = (nCount - iconSize) / 2;
+export const RendererWrapper = <T extends RendererProps>(renderer: SFC<T>) => {
+	const Renderer: SFC<T> = (props: T) => {
+		let newProps: T = Object.assign({}, props)
 
-    const pointList = [];
-    if (icon || titleVerticalAlign === "middle") {
-        pointList.push(<rect key={1} width={iconSize} height={iconSize} rx="2" ry="2" fill="#FFFFFF" x={iconXY} y={iconXY} />);
-    }
+		newProps.value = newProps.value || "https://qrlove.link"
+		newProps.level =
+			newProps.title || newProps.icon ? "H" : newProps.level || "M"
+		newProps.qrcode =
+			newProps.qrcode ||
+			encodeData({
+				text: newProps.value,
+				correctLevel: newProps.level,
+				typeNumber: -1,
+			})
+		newProps.styles = reactCSS(
+			merge(
+				{
+					default: renderer.defaultCSS,
+				},
+				{
+					default: props.styles,
+				}
+			)
+		)
 
-    if (icon) {
-        pointList.push(<image key={2} xlinkHref={icon} width={iconSize - 2} x={iconXY + 1} y={iconXY + 1} />);
-    }
+		return React.createElement(renderer, newProps)
+	}
+	return Renderer
+}
 
-    if (title) {
-        const svgWidth = styles.svg && styles.svg.width ? styles.svg.width.replace("px", "") : 300;
-        const titleFontSize = Number(nCount + nCount / 5 * 2) * (titleSize || fontSize || 12) / svgWidth;
-        const titleFontColor = titleColor || color || "#000000";
+export function drawIcon({
+	qrcode,
+	title,
+	titleSize,
+	titleColor,
+	titleAlign,
+	icon,
+	iconScale = 0.33,
+	styles,
+}: RendererProps) {
+	if (!qrcode) return []
+	if (!title && !icon) return null
 
-        const fontY = titleVerticalAlign === "middle"
-            ? (icon ? (iconXY + iconSize) : (nCount / 2 + titleFontSize * .5))
-            : Number(nCount + nCount / 5) - titleFontSize * .5;
+	const nCount = qrcode.getModuleCount()
+	const { fontSize, color, verticalAlign, ...titleStyle } = styles.title || {}
+	const titleVerticalAlign =
+		titleAlign || verticalAlign || (icon ? "bottom" : "middle")
 
-        pointList.push(<text key={3} x={nCount / 2} y={fontY} fill={titleFontColor} style={{ ...titleStyle, fontSize: titleFontSize }} textAnchor="middle">{title}</text>)
-    }
+	iconScale = iconScale > 0.33 ? 0.33 : iconScale
+	const iconSize = Number(nCount * iconScale)
+	const iconXY = (nCount - iconSize) / 2
 
-    return pointList;
+	const pointList = []
+	if (icon || titleVerticalAlign === "middle") {
+		pointList.push(
+			<rect
+				key={1}
+				width={iconSize}
+				height={iconSize}
+				rx="2"
+				ry="2"
+				fill="#FFFFFF"
+				x={iconXY}
+				y={iconXY}
+			/>
+		)
+	}
+
+	if (icon) {
+		pointList.push(
+			<image
+				key={2}
+				xlinkHref={icon}
+				width={iconSize - 2}
+				x={iconXY + 1}
+				y={iconXY + 1}
+			/>
+		)
+	}
+
+	if (title) {
+		const svgWidth =
+			styles.svg && styles.svg.width
+				? styles.svg.width.replace("px", "")
+				: 300
+		const titleFontSize =
+			(Number(nCount + (nCount / 5) * 2) *
+				(titleSize || fontSize || 12)) /
+			svgWidth
+		const titleFontColor = titleColor || color || "#000000"
+
+		const fontY =
+			titleVerticalAlign === "middle"
+				? icon
+					? iconXY + iconSize
+					: nCount / 2 + titleFontSize * 0.5
+				: Number(nCount + nCount / 5) - titleFontSize * 0.5
+
+		pointList.push(
+			<text
+				key={3}
+				x={nCount / 2}
+				y={fontY}
+				fill={titleFontColor}
+				style={{ ...titleStyle, fontSize: titleFontSize }}
+				textAnchor="middle"
+			>
+				{title}
+			</text>
+		)
+	}
+
+	return pointList
 }
